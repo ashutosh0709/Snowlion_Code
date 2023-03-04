8f0ef75fc995ac67a1241af035a05205e07e662f
ashutosh0709 <ashutoshraman07@gmail.com>
2023-03-04 13:02:05 +0530

    1st Final Commit

---
+++ PLEASE README FIRST.md
@@ -0,0 +1,21 @@
+Installation-Instruction: 
+#################################################################################
+1. Open terminal/cmd in the 'topicmodelling' folder.
+2. Check python version, must be between: 3.6 - 3.9 (Command-> 'python --version' or 'python3 --version'), otherwise, reinstall compatible python version.
+3. Run Command: python -m venv venv
+4. Run Command: .\venv\Scripts\activate
+5. Run Command: pip install -r requirements.txt
+6. Run command: exit
+#################################################################################
+
+
+
+
+
+After Installation, Running Code:
+#################################################################################
+1. Open terminal/cmd in 'topicmodelling' folder.
+2. Run: .\venv\Scripts\activate
+3. Run: python main.py   or   python3 main.py
+#################################################################################
+
---
+++ packages/preprocessing.py
@@ -0,0 +1,70 @@
+# Final output = Lematized list of words from article.
+
+###########################################################################################################################################################
+# IMPORTING AND DOWNLOADING THE NESSEARY LIBRARIES AT THE BEGINNING TO SAVE LOAD TIME OF MODEL LATER ON.
+
+import nltk 
+from nltk.corpus import stopwords
+nltk.download('stopwords', quiet=True) # quiet = true -> disables download prompt as it is clumsy in terminal.
+nltk.download('punkt', quiet=True)
+nltk.download('omw-1.4', quiet=True)
+from nltk.stem import WordNetLemmatizer 
+nltk.download('wordnet', quiet=True)
+import pandas as pd
+
+##################################################################################################################################################################
+# TAKE IN A TEXT FILE AND CONVERTS IT TO A STRING AND RETURNS THE STRING.
+
+def convertFileToVar(article_filename):
+
+    file = open(article_filename, encoding='utf-8')                                                                    
+    var = file.read()                                                                                  
+    file.close()
+    return var
+
+##################################################################################################################################################################
+# TAKES A STRING AND REPLACES A SPACE FOR ALL DIGITS AND SPECIAL-CHARS. LOWERS THE CASE OF ALL LETTERS, AND RETURNS THE STRING AND A COPY OF THAT STRING FOR SPACY LATER.
+
+def removespecialcharsAndNumbers(var):                                              
+    import re
+    text = re.sub(r'[^a-zA-Z]',' ',var) 
+    text = re.sub(r'\s+',' ',text)
+    text = text.lower()
+    text = re.sub(r'\d',' ',text)
+    return text 
+
+##################################################################################################################################################################
+# TAKES IN A BIG-STRING AND TOKENIZES IT TO WORDS AND REMOVES THE STOPWORDS, AND RETURN THE FINAL LIST OF WORDS.
+
+def tokenizeAndRemoveStopwordsAndSingleChar(text):               
+    
+
+    stop_words = stopwords.words('english') 
+    stop_words.extend(['us','etc','god','jesus','lord','evil','devil','man','israel','people','king','son','men','house','day','children','land','things','hand','earth','sons','son','jerusalem','city','father','bible','tlb'])
+
+    sentences = nltk.sent_tokenize(text) 
+    sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+
+    for i in range(len(sentences)): 
+        sentences[i] = [word for word in sentences[i] if word not in stop_words and len(word) > 1]
+
+    filtered_sentence = sentences
+    return filtered_sentence
+
+#################################################################################################################################################################
+# TAKES IN A LIST OF WORDS, LEMMATIZES IT ALL, AND RETURNS A LIST CONTAINING LEMMATIZED WORDS.
+
+def lemmatize(filtered_sentence):
+
+    lemmatizer = WordNetLemmatizer()
+
+    lematized_list = []
+    for word in filtered_sentence[0]:
+        lematized_list.append(lemmatizer.lemmatize(word ,  pos="v"))
+    lematized_list_short = list(set(lematized_list))
+    lematized_list_short = lematized_list
+
+
+    # print(len(lematized_list))
+    # print(len(lematized_list_short))
+    return lematized_list
